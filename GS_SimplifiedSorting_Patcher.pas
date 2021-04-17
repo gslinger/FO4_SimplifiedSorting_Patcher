35c589f4fb2ef7b4bb3be82a7c893a88171c97d2
GSL <24567123+UK-GSL@users.noreply.github.com>
2021-04-17 12:34:23 +0100

    Update GS_SimplifiedSorting_Patcher.pas

--- GS_SimplifiedSorting_Patcher.pas
+++ GS_SimplifiedSorting_Patcher.pas
@@ -5,7 +5,7 @@ uses 'lib\mxpf';
 
 // ==================================== Declare Constants Here ========================================== //
 const
-	blDebug     	  		= true;
+	blDebug     	  		= false;
 	blIgnoreBethesda  		= false;
 	blDefaultPluginState	= true;
 	// 
@@ -13,14 +13,16 @@ const
 	
 // ==================================== Declare Variables Here ========================================== //
 var 
-	validPlugins, tlSigsToLoad, kywdFilterAlch: TStringList;
+	tlFiles, validPlugins, tlSigsToLoad, kywdFilterAlch: TStringList;
+	i: integer;
 
 // ======================================== Main Function =============================================== //
 
 function Initialize: Integer;
 var
-	i, j: integer;
+	j: integer;
 	rec, f, g: IInterface;
+	sFiles: string;
 begin
 	DefaultOptionsMXPF;
 	InitializeMXPF;
@@ -28,25 +30,27 @@ begin
 	mxLoadMasterRecords := true;
 	mxSkipPatchedRecords := true;
 	mxLoadWinningOverrides := true;
-	// 
+	
+	// Creates various lists which are used by the script
 	CreateLists;
+	
 	// Filters only plugins with relevant records (defined by tlSigsToLoad) 
 	validPlugins := TStringList.Create;
 	FilterValidPlugins;
 	
-
-	for i := 0 to validPlugins.Count - 1 do begin
-		AddMessage(validPlugins[i]);
-	end;
-
-	
-		{for i := MaxRecordIndex downto 0 do begin
-			rec := GetRecord(i);
-			if not HasName(rec) then
-				//AddMessage(Name(rec));
-				continue;
-		end;}
+	// Show form to select which plugins to patch
 	ShowPluginSelect;
+	sFiles := tlFiles.CommaText;
+	
+	// Load all records
+	SetInclusions(sFiles);
+	LoadAllRecords;
+	
+	ProcessRecords;
+	
+	PrintMXPFReport;
+	FinalizeMXPF;
+	//ShowMessage('Patching Complete');
 end;
 
 // ==================================== Pre-Processing Functions =========================================
@@ -97,13 +101,51 @@ begin
 	end;
 end;
 
+// ===================================== Processing Functions ============================================ //
+
+procedure ProcessRecords();
+var
+	j: integer;
+	rec: IInterface;
+	sHeader: string;
+begin
+	// Filter Records
+	for i := MaxRecordIndex downto 0 do begin
+		rec := GetRecord(i);
+		if not HasName(rec) then begin
+			AddMessage(Format('[GS] - Filtered %s for having null name.', [Name(rec)]));
+			RemoveRecord(i);
+			continue;
+		end;
+		sHeader := GetElementEditValues(rec, 'Record Header\Signature');
+		
+		if sHeader = 'WEAP' then begin
+			FilterWeapon(rec);
+		end;
+	end;
+end;
+
+procedure FilterWeapon(e: IInterface);
+begin
+	AddMessage('1');
+end;
+
+
 // ======================================= Helper Functions ============================================== //
 
+// Check if a record has a model (MODL - Model Filename).
+function HasModel(e: IInterface): boolean;
+begin
+	Result := true;
+end;
+
+// Check if a record has a name (either null or blank).
 function HasName(e: IInterface): boolean;
 begin
 	Result := (ElementExists(e, 'FULL - Name')) or not (geev(e, 'FULL - Name') = '')
 end;
 
+// Checks if a record has any keywords from a list.
 function HasKeywordFromList(e: IInterface; keywords: TStringList): boolean;
 var 
 	j: integer;
@@ -117,6 +159,7 @@ begin
 	end;
 end;
 
+// Same as above but checks effects. 
 function HasEffectFromList(e: IInterface; effects: TStringList): boolean;
 var
 	j: integer;
@@ -130,6 +173,7 @@ begin
 	end;
 end;
 
+// Checks if a record has a certain effect 
 function HasEffect(e: IInterface; edid: String): boolean;
 var
 	effects, effect: IInterface;
@@ -154,12 +198,13 @@ const
 	spacing = 24;
 var
 	frm: TForm;
-	pnl: TPanel;
 	lastTop, contentHeight: Integer;
-	cbArray: Array[0..255] of TCheckBox;
+	cbArray: Array[0..1000] of TCheckBox;
 	lbl: TLabel;
 	sb: TScrollBox;
-	i: Integer;
+	btnOK: TButton;
+	//
+	j: Integer;
 	f: IInterface;
 begin
 	frm := TForm.Create(nil);
@@ -179,29 +224,43 @@ begin
 		// create label
 		lbl := TLabel.Create(sb);
 		lbl.Parent := sb;
-		lbl.Caption := 'ppppppppppppp';
+		lbl.Caption := 'Select Plugins you would like to Patch';
 		lbl.Left := 8;
 		lbl.Top := 8;
 		lbl.Width := 280;
 		lbl.WordWrap := true;
 		lastTop := lbl.Top + lbl.Height + 8 - spacing;
-
-		for i := 0 to validPlugins.Count - 1 do begin
-			f := FileByName(validPlugins[i]);
-			cbArray[i] := TCheckBox.Create(sb);
-			cbArray[i].Parent := sb;
-			cbArray[i].Caption := Format(' [%s] %s', [IntToHex(i, 2), validPlugins[i]]);
-			cbArray[i].Top := lastTop + spacing;
-			cbArray[i].Width := 260;
+		
+		// Create checkboxes for each plugin
+		for j := 0 to validPlugins.Count - 1 do begin
+			f := FileByName(validPlugins[j]);
+			cbArray[j] := TCheckBox.Create(sb);
+			cbArray[j].Parent := sb;
+			cbArray[j].Caption := Format(' [%s] %s', [IntToHex(j, 2), validPlugins[j]]);
+			cbArray[j].Top := lastTop + spacing;
+			cbArray[j].Width := 260;
 			lastTop := lastTop + spacing;
-			cbArray[i].Left := 12;
-			//cbArray[i].Checked := sl.IndexOf(GetFileName(f)) > -1;
+			cbArray[j].Left := 12;
+			cbArray[j].Checked := blDefaultPluginState;
 		end;
-		contentHeight := spacing*(i + 2) + 100;
+		
+		// Resizes form depending on how many plugins loaded
+		contentHeight := spacing * (j + 2) + 100;
 		if frm.Height > contentHeight then
 			frm.Height := contentHeight;
-
-		frm.ShowModal;
+		
+		// Create OK, Cancel buttons. (mtefunctions)
+		cModal(frm, frm, frm.Height - 70);
+	
+		tlFiles := TStringList.Create;
+	
+		if frm.ShowModal = mrOk then begin
+			for j := 0 to validPlugins.Count - 1 do begin
+				f := FileByName(validPlugins[j]);
+				if (cbArray[j].Checked) and (tlFiles.IndexOf(GetFileName(f)) = -1) then
+					tlFiles.Add(GetFileName(f));
+			end;
+		end;
 	Finally
 		frm.Free;
 	end;		
