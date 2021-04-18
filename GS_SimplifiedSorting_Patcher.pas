d9ce05f1fb9a70f8794c8cabd624c69a317057b0
GSL <24567123+UK-GSL@users.noreply.github.com>
2021-04-18 08:48:52 +0100

    tidy up

--- GS_SimplifiedSorting_Patcher.pas
+++ GS_SimplifiedSorting_Patcher.pas
@@ -1,33 +1,34 @@
 {
-	Hotkey: Ctrl+E
+	GS Simplified Sorting Patcher
+		by GS
+		
+	xEdit Script which automatically tags any items for use with Simplified Sorting. Can change tags for similar mods. 
 }
 
 unit UserScript;
 uses 'lib\mxpf';
 
-// ==================================== Declare Constants Here ========================================== //
 const
 	blDebug     	  		= true;
 	blIgnoreBethesda  		= false;
 	blDefaultPluginState	= true;
-	// 
+	blDeleteTags            = false;
 	excludeEsps       		= 'Fallout4.esm'#13'DLCCoast.esm'#13'DLCNukaWorld.esm'#13'DLCRobot.esm'#13'DLCworkshop01.esm'#13'DLCworkshop02.esm'#13'DLCworkshop03.esm';	
 	sAuthor                 = 'GS_SS_Patcher';
-	
-	// All tags will be here, I will put in txt file at end
-	
-	// Grenades/Mines 
+		
+	{ Tag Variables - Will probably add to file }
 	tUtilGrenade            = '[W9SIG]';  // Has Grenade Anim Type + A Substr from tlWeaponSignalGrenadeStrings
 	tTraps                  = '[W9TRP]';  // Has Grenade Anim Type + A Substr from tlWeaponTrapStrings
 	tGrenade                = '[W9GND]';  // Has Grenade Anim Type  (leftovers from above)
 	tMine             		= '[W9MNE]';          
-// =================================Declare Global Variables Here ========================================== //
 var 
-	tlFiles, validPlugins, tlSigsToLoad, tlWeaponSignalGrenadeStrings, fltrAlchemyKeywords, fltrAlchemyStrings, tlWeaponAnimMelee, fltrAlchemyStringsAllow, fltrAllowArmourRaces, fltrWeaponStrings: TStringList;
+	tlFiles, validPlugins, tlSigsToLoad, tlWeaponTrapStrings, tlWeaponSignalGrenadeStrings, fltrAlchemyKeywords, fltrAlchemyStrings, tlWeaponAnimMelee, fltrAlchemyStringsAllow, fltrAllowArmourRaces, fltrWeaponStrings: TStringList;
 	i: integer;
 	sHeader, sTag: string;
 
-// ======================================== Main Function =============================================== //
+{===================================================================================================================}
+{                                                  Main Function                                                    }
+{===================================================================================================================}
 
 function Initialize: Integer;
 var
@@ -43,40 +44,44 @@ begin
 	mxSkipPatchedRecords := true;
 	mxLoadWinningOverrides := true;
 	
-	// Creates various lists which are used by the script.
+	{ Creates various lists which are used by the script. }
 	CreateLists;
 	
-	// Filters only plugins with relevant records (defined by tlSigsToLoad).
+	{ Filters only plugins with relevant records (defined by tlSigsToLoad). }
 	AddMessage('[GS] - Checking Plugins for relevant records');
 	validPlugins := TStringList.Create;
 	FilterValidPlugins;
 	
-	// Show form to select which plugins to patch.
+	{ Show form to select which plugins to patch. }
 	ShowPluginSelect;
 	sFiles := tlFiles.CommaText;
 	
-	// Load all records.
+	{ Load all records. }
 	AddMessage('[GS] - Loading all relevant records');
 	SetInclusions(sFiles);
 	LoadAllRecords;
 	
-	// Abort if no records were found.
+	{ Abort Script if No Records found. }
 	if (MaxRecordIndex = -1) then begin
 		ShowMessage('No Relevant Records Found');
 		exit;
 	end;
 	
-	// Process the records, filtering + patching.
+	{ Process and Patch the records. }
 	ProcessRecords;
 	
-	// Finalizing Scripts and Tidy up.
+	{ Finish scripts and tidy records }
 	AddMessage('[GS] - Script is finishing, Patching was successful');
 	PrintMXPFReport;
 	FinalizeMXPF;
 	AddMessage('[GS] - Script has finished, Please remember to save patch');
 end;
 
-// ==================================== Pre-Processing Functions =========================================
+{===================================================================================================================}
+{                                              Pre-Process Functions                                                }
+{===================================================================================================================}
+
+{ Checks each plugin to see if it has any valid records. Validity defined by tlSigsToLoad. }
 procedure FilterValidPlugins();
 var
 	f, g: IInterface;
@@ -99,10 +104,10 @@ begin
 	end;	
 end;
 
+{ Creates various lists used by the script }
+{ TODO: Might be better to just use comma-separated strings with Pos()? }
 procedure CreateLists();
 begin
-	// Might be better to just use comma-separated strings with Pos(). 
-
 	tlSigsToLoad := TStringList.Create;
 		tlSigsToLoad.Add('WEAP');
 		//tlSigsToLoad.Add('ARMO');
@@ -111,7 +116,7 @@ begin
 		//tlSigsToLoad.Add('BOOK');
 		//tlSigsToLoad.Add('NOTE');
 		//tlSigsToLoad.Add('KEYM');
-		//tlSigsToLoad.Add('MISC');
+		tlSigsToLoad.Add('MISC');
 		
 	fltrAlchemyKeywords := TStringList.Create;
 		fltrAlchemyKeywords.Add('HC_IconColor_Red');
@@ -151,8 +156,14 @@ begin
 		tlWeaponSignalGrenadeStrings.Add('Signal');
 		tlWeaponSignalGrenadeStrings.Add('Beacon');
 		tlWeaponSignalGrenadeStrings.Add('Smoke');
+		tlWeaponSignalGrenadeStrings.Add('Relay');
+		
+	tlWeaponTrapStrings := TStringList.Create;
+		tlWeaponTrapStrings.Add('Trap');
+		tlWeaponTrapStrings.Add('Caltrop');
 end;
 
+{ Loads all records with a signature defined in tlSigsToLoad. }
 procedure LoadAllRecords();
 var
 	j: integer;
@@ -162,7 +173,7 @@ begin
 	end;
 end;
 
-
+{ Removes all records from Patch file }
 procedure ClearPatch();
 var 
 	j: integer;
@@ -171,14 +182,18 @@ begin
 		RemoveNode(GroupBySignature(mxPatchFile, tlSigsToLoad[j]));
 	end;
 end;
-// ===================================== Main Processing Procedure ============================================ //
 
+{===================================================================================================================}
+{                                             Main Process Function                                                 }
+{===================================================================================================================}
+
+{ This handles all processing and patching } 
 procedure ProcessRecords();
 var
 	j: integer;
 	rec: IInterface;
 begin
-	// Filter Records
+	{ This section removes records which should not be patched }
 	AddMessage('[GS] - The script will now filter and remove irrelevant or bad records');
 	for i := MaxRecordIndex downto 0 do begin
 		rec := GetRecord(i);
@@ -199,14 +214,13 @@ begin
 		end;
 	end;
 	
-	// Copy the records to patch file.
+	{ Copy the records to Patch File }
 	AddMessage('[GS] - Copying relevant records to patch file');
 	ClearPatch;
 	CopyRecordsToPatch;
 	
-	// Patch records and add tags.
+	{ This section edits the records by adding tags and various other tweaks }
 	AddMessage('[GS] - Patching records, may take a little while depending on selected load order size');
-	// 
 	for i := MaxRecordIndex downto 0 do begin
 		sTag := '';
 		rec := GetPatchRecord(i);
@@ -214,29 +228,32 @@ begin
 		if (sHeader = 'WEAP') then begin
 			PatchWeapon(rec);
 		end;
-		AddTag(rec, 'FULL - Name');
+		AddMessage(RemoveTags(geev(rec, 'FULL - Name')));
+		//AddTag(rec, 'FULL - Name');
 	end;	
 	
 	
 end;
 
-// ===================================== Filter Functions ============================================ //
+{===================================================================================================================}
+{                                                Filter Functions                                                   }
+{===================================================================================================================}
 
 procedure FilterWeapon(e: IInterface);
 begin
-	// Checks for Non Playable Tag
+	{ Checks if weapon has Non-Playable flag. }
 	if (GetElementEditValues(e, 'DNAM - Data\Flags\Not Playable') = '1') then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for having Non Playable tag', [sHeader, Name(e)]));
 		RemoveRecord(i);
 		exit;
 	end;
-	// Checks if weapon has model 
+	{ Checks if weapon has a model. }
 	if not (HasModel(e)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for having no model', [sHeader, Name(e)]));
 		RemoveRecord(i);
 		exit;
 	end;
-	// Check Editor IDs for certain Sub Strings
+	{ Checks weapon EDID for predefined strings. }
 	if (ElementContainsStrFromList(e, 'EDID - Editor ID', fltrWeaponStrings)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s based on excl/incl lists', [sHeader, Name(e)]));
 		RemoveRecord(i);
@@ -249,12 +266,12 @@ var
 	sRace: string;
 begin
 	sRace := GetElementEditValueTrimmed(e, 'RNAM - Race');
-	// Removes non-playable human armour
+	{ Checks if Armour has Non-Playable flag. }
 	if (sRace = 'HumanRace "Human"') and (IsNonPlayable(e)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for being Human + Non Playable', [sHeader, Name(e)]));
 		RemoveRecord(i);
 		exit;
-	// Removes armour for races not defined in list
+	{ Checks if Armour is usable by a race in the predefined list: fltrAllowArmourRaces. }
 	end else if (fltrAllowArmourRaces.IndexOf(sRace) = -1) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for not being allowed race', [sHeader, Name(e)]));
 		RemoveRecord(i);
@@ -264,7 +281,7 @@ end;
 
 procedure FilterAmmo(e: IInterface);
 begin
-	// Removes Non-Playable ammo
+	{ Checks if ammo has Non-Playable flag }
 	if (IsNonPlayable(e)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for having Non Playable tag', [sHeader, Name(e)]));
 		RemoveRecord(i);
@@ -274,19 +291,20 @@ end;
 
 procedure FilterAlchemy(e: IInterface);
 begin
-	// Might want to disable this if it removes script chems
+	{ Checks if Ingestible has a model. } 
+	{ NOTE: Might be a problem for script chems e.g. uninstallers? }
 	if not (HasModel(e)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for having no model', [sHeader, Name(e)]));
 		RemoveRecord(i);
 		exit;
 	end;
-	// Removes Non-Playable ammo
+	{ Checks if Ingestible has a keyword from a predefined list: fltrAlchemyKeywords. }
 	if (HasKeywordFromList(e, fltrAlchemyKeywords)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s for HC Keyword', [sHeader, Name(e)]));
 		RemoveRecord(i);
 		exit;
 	end;
-	// Has 2 sets of SubStr to check, 1 for allow, 1 for disallow.
+	{ Checks Ingestible EDID for certain substrings, inclusions: fltrAlchemyStringsAllow, exclusions: fltrAlchemyStrings. }
 	if (ElementContainsStrFromList(e, 'EDID - Editor ID', fltrAlchemyStrings)) and not (ElementContainsStrFromList(e, 'EDID - Editor ID', fltrAlchemyStringsAllow)) then begin
 		if blDebug then AddMessage(Format('[GS] - [%s] Filtered %s based on excl/incl lists', [sHeader, Name(e)]));
 		RemoveRecord(i);
@@ -295,53 +313,105 @@ begin
 end;
 
 
-// ===================================== Patch Functions ============================================ //
+{===================================================================================================================}
+{                                                 Patch Functions                                                   }
+{===================================================================================================================}
 
 procedure PatchWeapon(e: IInterface);
 var
 	sAnimType: string;
 begin
+	{ Checks animation types for various values. }
 	sAnimType := GetElementEditValues(e, 'DNAM - Data\Animation Type');
 	if (sAnimType = 'Grenade') then begin
 		if (ElementContainsStrFromList(e, 'FULL - Name', tlWeaponSignalGrenadeStrings)) then begin
-			
-		sTag := tGrenade;
+			sTag := tUtilGrenade;
+		end else if (ElementContainsStrFromList(e, 'FULL - Name', tlWeaponTrapStrings)) then begin
+			sTag := tTraps;
+		end else begin
+			sTag := tGrenade;
+		end;
 	end;
 end;
-// ======================================= Helper Functions ============================================== //
+
+{===================================================================================================================}
+{                                                Helper Functions                                                   }
+{===================================================================================================================}
+
+{ Adds a prefix tag to element from path, sTag is defined by Patch* functions. }
 procedure AddTag(e: IInterface; sPath: string);
 var
 	sCur: string;
 begin
+	
 	sCur := GetElementEditValues(e, sPath);
-	//DeleteTags;
-	SetElementEditValues(e, sPath, (sTag + ' ' + sCur));
+	if blDeleteTags then 
+		sCur := DeleteTags(sCur);
+	if (sTag <> '') then
+		SetElementEditValues(e, sPath, (sTag + ' ' + sCur));
 end;
 
-procedure DeleteTags(s: string);
+{ Removes existing tags, recursive to remove multiple. }
+function RemoveTags(s: string): string;
+var
+	sChars, sNew: string;
+	j, n, x: integer;
 begin
-	exit;
+	sChars := '[{(|]})|';
+	x := Length(sChars) Div 2;
+	n := 0;
+		
+	for j := 1 to x do begin
+		if (s[1] = sChars[j]) then begin
+			n := Pos(sChars[j+x], Copy(s, 2, Length(s))) + 1;
+			break;
+		end;
+	end;
+	
+	if (n = 0) then begin
+		Result := s;
+		exit;
+	end;
+	
+	sNew := Trim(Copy(s, n + 1, Length(s)));
+	
+	if (sNew = RemoveTags(sNew)) then
+		Result := sNew
+	else
+		Result := RemoveTags(sNew);
 end;
 
-// Check if a record has non playable record header tag.
+{ Removes component tags - RemoveTags should be run beforehand. }
+function RemoveCompTags(s: string): string;
+var
+	n: integer;
+begin
+	Result := s;
+	n := Pos('{', Copy(s, 2, Length(s)));
+	if (n >= 1) then
+		Result := Trim(Copy(s, 1, n));
+end;
+
+{ Checks if a record has a Non-Playable flag. }
 function IsNonPlayable(e: IInterface): boolean;
 begin
 	Result := false;
 	if (GetElementEditValues(e, 'Record Header\Record Flags\Non-Playable') = '1') then Result := true;
 end;
-// Check if a record has a model (MODL - Model Filename).
+
+{ Checks if a record has a model. }
 function HasModel(e: IInterface): boolean;
 begin
 	Result := Assigned(geev(e, 'Model\MODL - Model FileName'))
 end;
 
-// Check if a record has a name (either null or blank).
+{ Checks if a record has a name. }
 function HasName(e: IInterface): boolean;
 begin
 	Result := (ElementExists(e, 'FULL - Name')) or not (geev(e, 'FULL - Name') = '')
 end;
 
-// Checks if a record has any keywords from a list.
+{ Checks if a record has a keyword from a list. }
 function HasKeywordFromList(e: IInterface; keywords: TStringList): boolean;
 var 
 	j: integer;
@@ -355,7 +425,7 @@ begin
 	end;
 end;
 
-// Same as above but checks effects. 
+{ Checks if a record has an Effect from a list. }
 function HasEffectFromList(e: IInterface; effects: TStringList): boolean;
 var
 	j: integer;
@@ -369,7 +439,7 @@ begin
 	end;
 end;
 
-// Checks if a record has a certain effect.
+{ Checks if a record has a certain Effect. }
 function HasEffect(e: IInterface; edid: String): boolean;
 var
 	effects, effect: IInterface;
@@ -386,6 +456,7 @@ begin
 	end;
 end;
 
+{ GetElementEditValue but without the form ID. }
 function GetElementEditValueTrimmed(e: IInterface; sPath: string): string;
 var
 	sVal: string;
@@ -394,13 +465,13 @@ begin
 	Result := Copy(sVal, 1, (Pos('[', sVal) - 2));
 end; 
 
+{ Checks whether an element contains a SubStr. }
 function ElementContainsStr(e: IInterface; sPath, sStr: string): boolean;
 begin
-	Result := false;	
-	if (Pos(sStr, geev(e, sPath)) > 0) then
-		Result := true;
+	Result := (Pos(sStr, geev(e, sPath)) > 0);
 end;
 
+{ Checks whether an element contains a SubStr from a list. }
 function ElementContainsStrFromList(e: IInterface; sPath: string; tlStrs: TStringList): boolean;
 var
 	j: integer;
@@ -414,7 +485,9 @@ begin
 	end;
 end;
 
-// ======================================= GUI  ============================================== //
+{===================================================================================================================}
+{                                                      GUI                                                          }
+{===================================================================================================================}
 
 procedure ShowPluginSelect;
 const
@@ -426,7 +499,6 @@ var
 	lbl: TLabel;
 	sb: TScrollBox;
 	btnOK: TButton;
-	//
 	j: Integer;
 	f: IInterface;
 begin
@@ -438,13 +510,11 @@ begin
 		frm.BorderStyle := bsDialog;
 		frm.Caption := 'Multiple file selection';
 
-		// create scrollbox
 		sb := TScrollBox.Create(frm);
 		sb.Parent := frm;
 		sb.Align := alTop;
 		sb.Height := 500;
 
-		// create label
 		lbl := TLabel.Create(sb);
 		lbl.Parent := sb;
 		lbl.Caption := 'Select Plugins you would like to Patch';
@@ -454,7 +524,7 @@ begin
 		lbl.WordWrap := true;
 		lastTop := lbl.Top + lbl.Height + 8 - spacing;
 		
-		// Create checkboxes for each plugin
+		{ Create a checkbox for each plugin. }
 		for j := 0 to validPlugins.Count - 1 do begin
 			f := FileByName(validPlugins[j]);
 			cbArray[j] := TCheckBox.Create(sb);
@@ -467,16 +537,16 @@ begin
 			cbArray[j].Checked := blDefaultPluginState;
 		end;
 		
-		// Resizes form depending on how many plugins loaded
+		{ Resize form based on number of plugins loaded. }
 		contentHeight := spacing * (j + 2) + 100;
 		if frm.Height > contentHeight then
 			frm.Height := contentHeight;
 		
-		// Create OK, Cancel buttons. (mtefunctions)
+		{ Create Cancel, OK buttons - mtefunctions.pas }
 		cModal(frm, frm, frm.Height - 70);
 	
+		{ Populate tlFiles based on status of checkboxes. }
 		tlFiles := TStringList.Create;
-	
 		if frm.ShowModal = mrOk then begin
 			for j := 0 to validPlugins.Count - 1 do begin
 				f := FileByName(validPlugins[j]);
